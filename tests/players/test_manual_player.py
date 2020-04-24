dc244681a3eebbe22bdcd724072e4f6cbaccb34f
Jack <jack@tau2logic.com>
2020-04-25 00:24:05 +0200

    refactor test for manual_player

--- tests/players/test_manual_player.py
+++ tests/players/test_manual_player.py
@@ -7,30 +7,18 @@ from puzzlesearch.players.manual_player import ManualPlayer
 class TestManualPlayer(unittest.TestCase):
     def test_do_move_in_direction(self):
         player = ManualPlayer()
+        self.__assert_input_does_move("down", "down", player)
+        self.__assert_input_does_move("j", "down", player)
+        self.__assert_input_does_move("up", "up", player)
+        self.__assert_input_does_move("k", "up", player)
+        self.__assert_input_does_move("left", "left", player)
+        self.__assert_input_does_move("h", "left", player)
+        self.__assert_input_does_move("right", "right", player)
+        self.__assert_input_does_move("l", "right", player)
 
-        # down
-        with mock.patch("builtins.input", lambda: "down"):
-            self.assertEqual(player.do_move(None), "down")
-        with mock.patch("builtins.input", lambda: "j"):
-            self.assertEqual(player.do_move(None), "down")
-
-        # up
-        with mock.patch("builtins.input", lambda: "up"):
-            self.assertEqual(player.do_move(None), "up")
-        with mock.patch("builtins.input", lambda: "k"):
-            self.assertEqual(player.do_move(None), "up")
-
-        # left
-        with mock.patch("builtins.input", lambda: "left"):
-            self.assertEqual(player.do_move(None), "left")
-        with mock.patch("builtins.input", lambda: "h"):
-            self.assertEqual(player.do_move(None), "left")
-
-        # right
-        with mock.patch("builtins.input", lambda: "right"):
-            self.assertEqual(player.do_move(None), "right")
-        with mock.patch("builtins.input", lambda: "l"):
-            self.assertEqual(player.do_move(None), "right")
+    def __assert_input_does_move(self, input_, action, player):
+        with mock.patch("builtins.input", lambda: input_):
+            self.assertEqual(player.do_move(None), action)
 
     def test_do_move_repeat_when_unknown_iput(self):
         player = ManualPlayer()
